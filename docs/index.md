c6a4f12fb48288cea4bd406d2eac02d90570f446
Scaffolder <scaffolder@backstage.io>
2025-08-29 17:57:01 +0000

    initial commit

---
+++ docs/index.md
@@ -0,0 +1,18 @@
+# Documents for some-org-app-2
+
+This is a template app!
+
+For fun, it shows you a random dev excuse + a random cat
+
+# How to access the app?
+
+You can access the app by accessing this URL: `some-org-app-2-dev.test.com` 
+
+# Extra info?
+
+This application has two API endpoints:
+
+- `/api/v1/info`
+- `/api/v1/healthz`
+
+Here you could expand on what each of these endpoints do.
---
+++ src/app.py
@@ -0,0 +1,38 @@
+from flask import Flask, jsonify, render_template, send_from_directory
+import datetime
+import socket
+import random
+
+app = Flask(__name__)
+
+@app.route('/')
+
+def home():
+    return render_template(
+        'index.html',
+        cat_img=f"images-front/cat{random.randint(1, 7)}.gif",
+    )
+
+@app.route('/api/v1/info')
+
+def info():
+    return jsonify({
+        'time': datetime.datetime.now().strftime("%I:%M:%S%p  on %B %d, %Y"),
+        'hostname': socket.gethostname(),
+        'deployed_on': 'kubernetes',
+    })
+
+@app.route('/api/v1/healthz')
+
+def health():
+    # Do an actual check here
+    return jsonify({'status': 'up'}), 200
+
+
+@app.route('/images-front/<filename>')
+def images_frontend(filename):
+    return send_from_directory('templates/img', filename)
+
+if __name__ == '__main__':
+
+    app.run(host="0.0.0.0")
